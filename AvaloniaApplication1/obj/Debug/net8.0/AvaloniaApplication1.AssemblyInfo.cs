0007ff926c91320b74c12269eb3c775a169efe0d
Arman Aslanyan <armo2004.16@gmail.com>
2025-07-09 15:19:24 +0400

    Fixed LargeLabel layout and fit 8 + 2 buttons in main area

--- AvaloniaApplication1/obj/Debug/net8.0/AvaloniaApplication1.AssemblyInfo.cs
+++ AvaloniaApplication1/obj/Debug/net8.0/AvaloniaApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AvaloniaApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fad185285ef78933677085cfb6e01c699ee3d972")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e0d4f0c2762bc5e42d7329a1c52cb3a593f75925")]
 [assembly: System.Reflection.AssemblyProductAttribute("AvaloniaApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AvaloniaApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
